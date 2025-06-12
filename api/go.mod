927795f8eae3fecfe6647c851d31ade187da2f64
JedBorseth <jedborseth@gmail.com>
2025-06-12 16:37:32 -0700

    testing if vercel can use go at all

---
+++ api/go.mod
@@ -0,0 +1,3 @@
+module jedborseth/uno
+
+go 1.23.4
---
+++ api/index.go
@@ -0,0 +1,19 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+)
+ 
+func Handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Received request:", r.Method, r.URL.Path)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	response := `{"message": "Hello, World!"}`
+	_, err := w.Write([]byte(response))
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	fmt.Println("Response sent successfully")
+}
