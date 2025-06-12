8a9199dde8f62742aa01c91a26eb1a89453ee4d4
JedBorseth <jedborseth@gmail.com>
2025-06-12 16:49:39 -0700

    multiple routes

--- api/go.mod
+++ api/go.mod
@@ -1,3 +1,5 @@
 module jedborseth/uno
 
 go 1.23.4
+
+require github.com/gorilla/websocket v1.5.3 // indirect
--- api/ws.go
+++ api/ws.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 	"net/http"
 )
- 
-func Handler(w http.ResponseWriter, r *http.Request) {
+
+
+func ws(w http.ResponseWriter, r *http.Request) {
+
 	fmt.Println("Received request:", r.Method, r.URL.Path)
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	response := `{"message": "Hello, World!"}`
+	response := `{"message": "Hello, WebSocket!"}`
 	_, err := w.Write([]byte(response))
 	if err != nil {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
