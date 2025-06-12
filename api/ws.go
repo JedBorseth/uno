9e03ad30a647cd04a0b66401c46a000460f66a9d
JedBorseth <jedborseth@gmail.com>
2025-06-12 16:52:59 -0700

    multiple go files

--- api/ws.go
+++ api/ws.go
@@ -6,12 +6,11 @@ import (
 )
 
 
-func ws(w http.ResponseWriter, r *http.Request) {
-
+func WS(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Received request:", r.Method, r.URL.Path)
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	response := `{"message": "Hello, WebSocket!"}`
+	response := `{"message": "Hello, WS!"}`
 	_, err := w.Write([]byte(response))
 	if err != nil {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
